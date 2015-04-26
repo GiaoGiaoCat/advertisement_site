f0bddc4cc4eb97ad78754c321867aa268dc82a5d
Victor Wang <wjp2013@gmail.com>
2015-04-26 13:52:34 +0800

    change puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -1,31 +1,56 @@
 #!/usr/bin/env puma
+require 'puma'
 
-environment "production"
+# http://demo.gitlab.com/gitlab/gitlabhq/blob/master/config/puma.rb.example
+# Start Puma with next command:
+# bundle exec puma
+
+# Set the environment in which the rack's app will run.
+#
+# The default is “development”.
+#
+environment 'production'
+
+
+# Daemonize the server into the background. Highly suggest that
+# this be combined with “pidfile” and “stdout_redirect”.
+#
+# The default is “false”.
+#
 daemonize true
 
-workers 0
-threads 0, 16
+workers 4
+threads 8, 64
 
 wd = File.expand_path('../../', __FILE__)
-tmp_path = File.join(wd, 'log')
+tmp_path = File.join(wd, 'tmp')
+log_path = File.join(wd, 'log')
 Dir.mkdir(tmp_path) unless File.exist?(tmp_path)
 
-bind  "unix:///var/run/topfun.sock"
-pidfile File.join(tmp_path, 'puma.pid')
-state_path File.join(tmp_path, 'puma.state')
-stdout_redirect File.join(tmp_path, 'puma.out.log'), File.join(tmp_path, 'puma.err.log'), true
+# Store the pid of the server in the file at `path`.
+pidfile File.join(tmp_path, 'pids', 'puma.pid')
+
+# Use `path` as the file to store the server info state. This is
+# used by `pumactl` to query and control the server.
+state_path File.join(tmp_path, 'sockets', 'puma.state')
+
+# Redirect STDOUT and STDERR to files specified.
+stdout_redirect File.join(log_path, 'puma.out.log'), File.join(log_path, 'puma.err.log'), true
+
+# Bind the server.
+bind "unix:///var/run/site.master.sock"
 
 preload_app! #utilizing copy-on-write
 activate_control_app
+
+# Thread safety
+# https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
+# https://devcenter.heroku.com/articles/concurrency-and-database-connections#connection-pool
 on_worker_boot do
-  # worker specific setup
-  ActiveRecord::Base.connection_pool.disconnect!
   ActiveSupport.on_load(:active_record) do
     config = ActiveRecord::Base.configurations[Rails.env] ||
-      Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10
-    config['pool'] = ENV['MAX_THREADS'] || 16
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 64
     ActiveRecord::Base.establish_connection(config)
   end
 end
-
